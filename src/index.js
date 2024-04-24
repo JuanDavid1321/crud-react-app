b2182be0249c830aa651453edb95d6bafc01be27
JuanDavid1321 <jdcarvajal@unicauca.edu.co>
2024-04-24 16:59:25 -0500

    feat: add dynamic routes for navigation

--- src/index.js
+++ src/index.js
@@ -1,46 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import Root from './routes/Root';
-import DocumentTypesManagement from './routes/DocumentTypesManagement';
-import RolesManagement from './routes/RolesManagement';
-import UsersManagement from './routes/UsersManagement';
-import Home from './routes/Home';
-import Login from './routes/Login';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css"
 
-const router = createBrowserRouter([
-  {
-    element: <Root/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: "documents",
-        element: <DocumentTypesManagement/>,
-      },
-      {
-        path: "roles",
-        element: <RolesManagement/>,
-      },
-      {
-        path: "users",
-        element: <UsersManagement/>,
-      },
-      {
-        path: "login",
-        element: <Login/>
-      },
-    ]
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
