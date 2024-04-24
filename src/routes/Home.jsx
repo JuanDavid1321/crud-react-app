0d3cd7df3faaa3ee7709ebb2319a410cc15bbd6f
Juan David Carvajal Cucuñame <jdcc1308@gmail.com>
2024-04-24 16:21:44 -0500

    feat: create a dropdown navbar with styles (#3)
    * feat: create Login page and add this element to router
    
    * feat: creat Table component for management views
    
    * feat: create a dropdown navbar (#2)
    
    * feat: create Login page and add this element to router
    
    * feat: creat Table component for management views
    
    * added bottom border to submenu
    
    ---------
    
    Co-authored-by: JuanDavid1321 <jdcarvajal@unicauca.edu.co>

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header/Header";
+
 const Home = () => {
     return ( 
         <div className="Home">
+            <Header />
             <h1>Bienvenido!</h1>
         </div>
     );
 }
- 
 export default Home;
