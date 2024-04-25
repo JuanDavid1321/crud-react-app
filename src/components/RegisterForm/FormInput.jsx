422ebe8844104c597b723f319a376cac53711018
jdcarvajal <jdcc1308@gmail.com>
2024-04-25 15:56:27 -0500

    refactor: use CSS modules in FormInput, update RegisterForm styles, and add error messages to registerInputsData
    This commit includes the following changes:
    
    * In `FormInput.jsx`, the import statement has been

--- src/components/RegisterForm/FormInput.jsx
+++ src/components/RegisterForm/FormInput.jsx
@@ -1,11 +1,13 @@
-import "./formInput.css"
+import styles from "./FormInput.module.css"
 
 const FormInput = (props) => { //props used for a lot of props from the father
-    const {label, onChange, id, ...inputProps} = props;
+    const {label, errorMessage, onChange, id, ...inputProps} = props;
     return ( 
-        <div className="formInput">
-            <label>{label}</label>
-            <input {...inputProps} onChange={onChange}/>
+        <div className={styles.formInput}>
+            <label className={styles.labelGeneric}>{label}</label>
+            <input required className={styles.inputGeneric} {...inputProps} onChange={onChange}/>
+            <span className={styles.errorMessageGeneric}>Campo obligatorio!</span>
+            <span className={styles.errorMessageGeneric}>{errorMessage}</span>
         </div>
     );
 }
