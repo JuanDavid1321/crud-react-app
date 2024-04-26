ecce6ba45e28ee7bc8c01c1a03866526c7d65e06
jdcarvajal <jdcc1308@gmail.com>
2024-04-25 19:05:48 -0500

    ImageInput component integrated in RegisterForm component GUI

--- src/components/RegisterForm/ImageInput.jsx
+++ src/components/RegisterForm/ImageInput.jsx
@@ -1,17 +1,22 @@
 import styles from "./ImageInput.module.css"
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 const ImageInput = () => {
     return ( 
         <div className={styles.imageInputContainer}>
             <label>Subir foto</label>
-            <label htmlFor="file" ></label>
-            <input 
-                required 
-                type="file" 
-                id="file" 
-                name="file" 
-                accept=".jpg,.jpeg,.png"
-            />
+            <div className={styles.fileInputContainer}>
+                <label htmlFor="file"> <DriveFolderUploadOutlinedIcon/></label>
+                <label htmlFor="file"> Seleccione un archivo</label>
+                <input 
+                    required 
+                    className={styles.imageInput}
+                    type="file" 
+                    id="file" 
+                    name="file" 
+                    accept=".jpg,.jpeg,.png"
+                />
+            </div>
         </div>
     );
 }
