f9a63269fc4fa184d712ca2d7d92b7f484fd4999
JuanDavid1321 <jdcarvajal@unicauca.edu.co>
2024-04-25 23:32:21 -0500

    mui icons for home and logout added to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar";
 import styles from "./Header.module.css"
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+
 const Header = () => {
     return ( 
         <header className={styles.header}>
             <Link to="/" style={{textDecoration: 'none'}}>
-                <p>Inicio</p>
+                <HomeRoundedIcon className={styles.home} />
             </Link>
             <Navbar/>
+            <Link to="/login">
+                <LogoutIcon className={styles.logOut}/>
+            </Link>
         </header>
     );
 }
