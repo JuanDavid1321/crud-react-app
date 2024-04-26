341add292a2e257c025e1cad8efb869cbb5e499a
jdcarvajal <jdcc1308@gmail.com>
2024-04-26 13:58:30 -0500

    GoogleButtonSeparator component created

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,15 +1,23 @@
+import GoogleButtonSeparator from '../GoogleButtonSeparator/GoogleButtonSeparator';
 import styles from './LoginForm.module.css'
+import GoogleButton from 'react-google-button'
 
 const LoginForm = ({formTitle}) => {
     return ( 
         <form action="" className={styles.form}>
-                <h2 className={styles.formTitle}>{formTitle}</h2>
-                <div className={styles.inputsContainer}>
-                    
-                </div>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.submitButton}>Guardar</button>
-                </div>
+            <h2 className={styles.formTitle}>{formTitle}</h2>
+            <div className={styles.inputsContainer}>
+                
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.submitButton}>Guardar</button>
+            </div>
+
+            {/* Separator component for Google Signin button*/}
+            <GoogleButtonSeparator/>
+            <div className={styles.googleButtonContainer}>
+                <GoogleButton style={{marginBottom: "30px"}} onClick={()=>console.log('Clicked!')} />
+            </div>
         </form>
     );
 }
