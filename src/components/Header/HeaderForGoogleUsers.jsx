679ba0dde755ce5b1d8c8c87fc3a2c7b3269e3a7
jdcarvajal <jdcc1308@gmail.com>
2024-04-27 17:04:34 -0500

    HeaderForGoogleUsers component created, now in Home component when user login through Google Auth, this new component will be render rather than Header component

---
+++ src/components/Header/HeaderForGoogleUsers.jsx
@@ -0,0 +1,11 @@
+import styles from "./HeaderForGoogleUsers.module.css";
+
+const HeaderForGoogleUsers = () => {
+    return (
+        <div>
+            <h1>sada</h1>
+        </div>
+    );
+};
+
+export default HeaderForGoogleUsers;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,14 +2,24 @@ import "./Home.css";
 import { useGoogleAuth } from "../../context/GoogleAuthContext";
 import Header from "../../components/Header/Header";
 import WelcomeCard from "../../components/WelcomeCard/WelcomeCard";
+import HeaderForGoogleUsers from "../../components/Header/HeaderForGoogleUsers";
 
 const Home = () => {
-    const { googleUser } = useGoogleAuth();
+    const { googleUser, isLoggedInWithGoogle } = useGoogleAuth();
     return (
-        <div className="home">
-            <Header />
-            <WelcomeCard userName={googleUser.displayName} />
-        </div>
+        <>
+            {isLoggedInWithGoogle ? (
+                <div className="home">
+                    <HeaderForGoogleUsers />
+                    <WelcomeCard userName={googleUser.displayName} />
+                </div>
+            ) : (
+                <div className="home">
+                    <Header />
+                    <WelcomeCard userName={"Visitante"} />
+                </div>
+            )}
+        </>
     );
 };
 export default Home;
