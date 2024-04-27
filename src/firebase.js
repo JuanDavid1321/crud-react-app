c67835805e88bd4b3c209e645d0f739c94e48e61
jdcarvajal <jdcc1308@gmail.com>
2024-04-27 18:33:07 -0500

    hotfix: change deprecated dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBstGYXU7uvkUhWLBJ2ZpQ8NIzjx9TAQrE",
-    authDomain: "crud-app-electiva.firebaseapp.com",
-    projectId: "crud-app-electiva",
-    storageBucket: "crud-app-electiva.appspot.com",
-    messagingSenderId: "1003216541194",
-    appId: "1:1003216541194:web:92f05fcb9651fd30fab1f3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
