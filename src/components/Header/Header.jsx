4628837c7c20ffecac5cd739fea61df682415027
jdcarvajal <jdcc1308@gmail.com>
2024-04-27 17:36:40 -0500

    HeaderForGoogleUsers component logic completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,19 @@
 import styles from "./Header.module.css";
-import { useGoogleAuth } from "../../context/GoogleAuthContext"; //for loging out
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 const Header = () => {
-    const { googleLogOut } = useGoogleAuth(); //destructuring  assignment to access the function 'logOut' from GoogleAuthContext
-
     return (
         <header className={styles.header}>
-            <Link to="/" style={{ display: "flex" }}>
-                <HomeRoundedIcon sx={{ fontSize: 30, paddingLeft: "5vw" }} />
+            <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+                <HomeRoundedIcon sx={{ fontSize: 34, paddingLeft: "5vw" }} />
             </Link>
             <Navbar />
-            <Link to="/login" style={{ display: "flex" }}>
-                <LogoutIcon sx={{ fontSize: 30 }} onClick={googleLogOut} />
+            <Link to="/login" style={{ display: "flex", alignItems: "center" }}>
+                {/* implement onClick={logOut} for mailAndPasswordAuth */}
+                <LogoutIcon sx={{ fontSize: 34 }} />
             </Link>
         </header>
     );
