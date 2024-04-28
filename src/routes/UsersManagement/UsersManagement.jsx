d71bae90192ec64ae8a8d86ec54a732dfcb53a6e
jdcarvajal <jdcc1308@gmail.com>
2024-04-28 17:26:18 -0500

    root styles updated

--- src/routes/UsersManagement/UsersManagement.jsx
+++ src/routes/UsersManagement/UsersManagement.jsx
@@ -1,13 +1,17 @@
+import styles from "./UsersManagement.module.css";
 import Header from "../../components/Header/Header";
 import NewUserModal from "../../components/Modals/NewUserModal";
-import NewUserForm from "../../components/NewUserForm/NewUserForm";
+import UsersTable from "../../components/Tables/UsersTable";
 
 const UsersManagement = () => {
     return (
-        <>
+        <div className={styles.usersManagementContainer}>
             <Header />
             <NewUserModal />
-        </>
+            <div className={styles.usersTableContainer}></div>
+            <UsersTable />
+            <div />
+        </div>
     );
 };
 export default UsersManagement;
