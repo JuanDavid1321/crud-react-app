c7d97954db1a400b8cfc23486e4d5037dca5d94f
jdcarvajal <jdcc1308@gmail.com>
2024-04-28 16:17:23 -0500

    mui Modal component added to UserManagement route

--- src/routes/UsersManagement/UsersManagement.jsx
+++ src/routes/UsersManagement/UsersManagement.jsx
@@ -1,11 +1,12 @@
 import Header from "../../components/Header/Header";
+import NewUserModal from "../../components/Modals/NewUserModal";
 import NewUserForm from "../../components/NewUserForm/NewUserForm";
 
 const UsersManagement = () => {
     return (
         <>
             <Header />
-            <NewUserForm />
+            <NewUserModal />
         </>
     );
 };
