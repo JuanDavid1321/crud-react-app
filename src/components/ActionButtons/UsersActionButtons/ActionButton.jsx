4aecfe29f9a35805636a3d4507c7af23bf2817d0
jdcarvajal <jdcc1308@gmail.com>
2024-04-29 14:06:28 -0500

    UsersActionButtons component use ActionButton component and pass props (buttonType and IconComponent) to this child

--- src/components/ActionButtons/UsersActionButtons/ActionButton.jsx
+++ src/components/ActionButtons/UsersActionButtons/ActionButton.jsx
@@ -1,33 +1,69 @@
-const ActionButton = ({ style, IconComponent }) => {
-    const readButton = {
-        background: "#08A6C1",
+// const ActionButton = ({ style, IconComponent }) => {
+//     const readButton = {
+//         background: "#08A6C1",
+//         cursor: "pointer",
+//         borderRadius: "10px",
+//         padding: "10px",
+//         textAlign: "center",
+//     };
+
+//     const updateButton = {
+//         background: "#FF9C26",
+//         cursor: "pointer",
+//         borderRadius: "10px",
+//         padding: "10px",
+//         textAlign: "center",
+//     };
+
+//     const deleteButton = {
+//         background: "#DE3C14",
+//         cursor: "pointer",
+//         borderRadius: "10px",
+//         padding: "10px",
+//         textAlign: "center",
+//     };
+
+//     return (
+//         <>
+//             <button style={updateButton}>{IconComponent}</button>
+//         </>
+//     );
+// };
+
+// export default ActionButton;
+import React from "react";
+
+const buttonStyles = {
+    read: {
+        background: "none",
+        color: "#08A6C1",
+        cursor: "pointer",
+        borderRadius: "50%",
+        padding: "10px",
+        textAlign: "center",
+    },
+    update: {
+        background: "none",
+        color: "#FF9C26",
         cursor: "pointer",
         borderRadius: "10px",
         padding: "10px",
         textAlign: "center",
-    };
-
-    const updateButton = {
-        background: "#FF9C26",
+    },
+    delete: {
+        background: "none",
+        color: "#DE3C14",
         cursor: "pointer",
         borderRadius: "10px",
         padding: "10px",
         textAlign: "center",
-    };
+    },
+};
 
-    const deleteButton = {
-        background: "#DE3C14",
-        cursor: "pointer",
-        borderRadius: "10px",
-        padding: "10px",
-        textAlign: "center",
-    };
+const ActionButton = ({ buttonType, IconComponent }) => {
+    const appliedStyles = buttonStyles[buttonType];
 
-    return (
-        <>
-            <button style={updateButton}>{IconComponent}</button>
-        </>
-    );
+    return <button style={appliedStyles}>{IconComponent}</button>;
 };
 
 export default ActionButton;
--- src/components/ActionButtons/UsersActionButtons/UsersActionButtons.jsx
+++ src/components/ActionButtons/UsersActionButtons/UsersActionButtons.jsx
@@ -1,7 +1,23 @@
+import ActionButton from "./ActionButton";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const UsersActionButtons = () => {
     return (
         <>
-            <div>sad</div>
+            <ActionButton
+                buttonType={"read"}
+                IconComponent={<RemoveRedEyeIcon />}
+            />
+            <ActionButton
+                buttonType={"update"}
+                IconComponent={<ModeEditIcon />}
+            />
+            <ActionButton
+                buttonType={"delete"}
+                IconComponent={<DeleteIcon />}
+            />
         </>
     );
 };
