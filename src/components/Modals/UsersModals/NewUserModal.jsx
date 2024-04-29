8aee1468845c057e3b3b5fa828af13ff6238b66e
jdcarvajal <jdcc1308@gmail.com>
2024-04-29 14:14:25 -0500

    C:/Program Files/Git/utils/ folder now contains actionButtonStyles and modalStyles objects, this action improves readability of components

--- src/components/Modals/UsersModals/NewUserModal.jsx
+++ src/components/Modals/UsersModals/NewUserModal.jsx
@@ -4,20 +4,7 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import NewUserForm from "../../NewUserForm/NewUserForm";
 import PersonAddRoundedIcon from "@mui/icons-material/PersonAddRounded";
-
-const modalStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "70%",
-    bgcolor: "background.paper",
-    p: 4,
-    padding: "20px 60px",
-    borderRadius: "20px",
-    boxShadow:
-        "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
-};
+import { modalStyle } from "../../../utils/modalStyles";
 
 export default function NewUserModal() {
     const [open, setOpen] = React.useState(false);
