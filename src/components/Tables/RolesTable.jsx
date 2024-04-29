29e33e34dc24244f97321b175ba68c14efdd073c
jdcarvajal <jdcc1308@gmail.com>
2024-04-29 18:45:17 -0500

    delete read button in RolesManagement and IDTypesManagement components. Also was configured flex 1 for responsive table

--- src/components/Tables/RolesTable.jsx
+++ src/components/Tables/RolesTable.jsx
@@ -1,37 +1,23 @@
 import { DataGrid } from "@mui/x-data-grid";
-import UsersActionButtons from "../ActionButtons/UsersActionButtons/UsersActionButtons";
+import RolesActionButtons from "../ActionButtons/RolesActionButtons/RolesActionButtons";
 
 const RolesTable = () => {
     const columns = [
-        { field: "id", headerName: "ID", width: 60 },
-        { field: "firstName", headerName: "First name", width: 130 },
-        { field: "lastName", headerName: "Last name", width: 130 },
-        {
-            field: "age",
-            headerName: "Age",
-            type: "number",
-            width: 90,
-        },
+        { field: "id", headerName: "ID", flex: 1 },
+        { field: "role", headerName: "Rol", flex: 1 },
         {
             field: "actions",
             headerName: "Acciones",
-            width: 320,
+            flex: 1,
             renderCell: (params) => {
-                return <UsersActionButtons />;
+                return <RolesActionButtons />;
             },
         },
     ];
 
     const rows = [
-        { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-        { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-        { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-        { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-        { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-        { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-        { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-        { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-        { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+        { id: 1, role: "Usuario" },
+        { id: 2, role: "Moderador" },
     ];
 
     return (
--- src/components/Tables/UsersTable.jsx
+++ src/components/Tables/UsersTable.jsx
@@ -3,19 +3,13 @@ import UsersActionButtons from "../ActionButtons/UsersActionButtons/UsersActionB
 
 const UsersTable = () => {
     const columns = [
-        { field: "id", headerName: "ID", width: 60 },
-        { field: "firstName", headerName: "First name", width: 130 },
-        { field: "lastName", headerName: "Last name", width: 130 },
-        {
-            field: "age",
-            headerName: "Age",
-            type: "number",
-            width: 90,
-        },
+        { field: "id", headerName: "ID", flex: 1 },
+        { field: "firstName", headerName: "First name", flex: 1 },
+        { field: "lastName", headerName: "Last name", flex: 1 },
         {
             field: "actions",
             headerName: "Acciones",
-            width: 320,
+            flex: 1,
             renderCell: (params) => {
                 // const user = users.find((u) => u.id === params.row.id);
                 // return <ActionButtons user={user} />;
@@ -25,15 +19,15 @@ const UsersTable = () => {
     ];
 
     const rows = [
-        { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-        { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-        { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-        { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-        { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-        { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-        { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-        { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-        { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+        { id: 1, lastName: "Snow", firstName: "Jon" },
+        { id: 2, lastName: "Lannister", firstName: "Cersei" },
+        { id: 3, lastName: "Lannister", firstName: "Jaime" },
+        { id: 4, lastName: "Stark", firstName: "Arya" },
+        { id: 5, lastName: "Targaryen", firstName: "Daenerys" },
+        { id: 6, lastName: "Melisandre", firstName: null },
+        { id: 7, lastName: "Clifford", firstName: "Ferrara" },
+        { id: 8, lastName: "Frances", firstName: "Rossini" },
+        { id: 9, lastName: "Roxie", firstName: "Harvey" },
     ];
 
     return (
