d72eb492f93e25f6f540b232692f2256af6d4ce8
Juan David Carvajal Cucuñame <jdcc1308@gmail.com>
2024-04-29 16:39:20 -0500

    feat: implement users management table (#11)
    * root styles updated
    
    * NewUserModal now have a custom button (basic styles)
    
    * Login route now has the email and password inputs
    
    * UsersManagement styles modified, now the title and create a new user button are inside the user table container
    
    * basic structure for action buttons in UsersTable component
    
    * new structure for managing action buttons. An ActionButton component is going to update dinamically each button in the UsersTable component
    
    * UsersActionButtons component use ActionButton component and pass props (buttonType and IconComponent) to this child
    
    * C:/Program Files/Git/utils/ folder now contains actionButtonStyles and modalStyles objects, this action improves readability of components
    
    * ActionButton component now receives a ModalComponent prop
    
    * NewUserModal component change to CreateActionModal, this allows reusability for other routes (roles and ID types). Because now the CreateActionModal component receives IconForButton and ModalContent props
    
    * hotfix: change the name for CreateActionModal component, now it´s called CreateActionButtonAndModal
    
    * now the UsersActionButtons component pass a conditional prop for delete action button

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,18 +1,47 @@
 import styles from "./LoginForm.module.css";
+import { useState } from "react";
+import GeneralInputs from "../NewUserForm/GeneralInputs";
 import GoogleAuth from "../GoogleAuth/GoogleAuth";
+import { emailInput, passwordInput } from "../../utils/loginInputsData";
 
 const LoginForm = ({ formTitle }) => {
+    // useState hook for onChange event in the input elements
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    const onChange = (e) => {
+        /*
+         *   Spread the values, that means take all the current values
+         *   and add/change only the one we are handling in this onChange function
+         */
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+    console.log(values);
+
     return (
         <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
             <h2 className={styles.formTitle}>{formTitle}</h2>
-            <div className={styles.inputsContainer}></div>
+            <div className={styles.inputsContainer}>
+                <GeneralInputs
+                    {...emailInput}
+                    value={values.email}
+                    onChange={onChange}
+                />
+                <GeneralInputs
+                    {...passwordInput}
+                    value={values.password}
+                    onChange={onChange}
+                />
+            </div>
             <div className={styles.buttonContainer}>
                 <button type="submit" className={styles.submitButton}>
                     Enviar
                 </button>
             </div>
 
-            <GoogleAuth></GoogleAuth>
+            <GoogleAuth />
         </form>
     );
 };
--- src/routes/RolesManagement/RolesManagement.jsx
+++ src/routes/RolesManagement/RolesManagement.jsx
@@ -1,14 +1,28 @@
+import styles from "./RolesManagement.module.css";
 import Header from "../../components/Header/Header";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import CreateActionButtonAndModal from "../../components/Modals/CreateActionButtonAndModal";
+import Table from "../../components/Table/Table";
 
 const RolesManagement = () => {
-    return ( 
+    return (
         <>
-            <Header/>
-            <div>
-                <h2>Gestionar roles</h2>
+            <Header />
+            <div className={styles.rolesManagementContainer}>
+                <div className={styles.rolesTableContainer}>
+                    <div className={styles.rolesTitleAndButtonContainer}>
+                        <h2>Gestionar roles</h2>
+                        <CreateActionButtonAndModal
+                            IconForButton={<AdminPanelSettingsIcon />}
+                            textForButton={"Nuevo rol"}
+                            ModalContent={<></>}
+                        />
+                    </div>
+                    <Table ActionButtons={<></>} />
+                </div>
             </div>
-        </> 
+        </>
     );
-}
- 
+};
+
 export default RolesManagement;
