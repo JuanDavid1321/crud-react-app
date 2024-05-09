94e6cab8e4da91a1dbfec8c53f2f0acd2a961114
jdcarvajal <jdcc1308@gmail.com>
2024-05-09 17:50:03 -0500

    create NewIDDocumentTypeForm folder and it´s children

---
+++ src/components/Forms/NewIDDocumentTypeForm/NewIDDocumentTypeForm.jsx
@@ -0,0 +1,11 @@
+import styles from "./NewIDDocumentTypeForm.module.css";
+
+const NewIDDocumentTypeForm = () => {
+    return (
+        <>
+            <h1>asd</h1>
+        </>
+    );
+};
+
+export default NewIDDocumentTypeForm;
