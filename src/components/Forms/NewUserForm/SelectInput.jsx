be40f28f019e97649345a52828609276565d8b06
jdcarvajal <jdcc1308@gmail.com>
2024-05-09 15:48:30 -0500

    add state and useEffect to fetch roles and idDocumentTypes options for SelectInput in NewUserForm

--- src/components/Forms/NewUserForm/SelectInput.jsx
+++ src/components/Forms/NewUserForm/SelectInput.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
 import styles from "./SelectInput.module.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const SelectInput = (props) => {
+    const [rolesOptions, setRolesOptions] = useState([]);
+    const [idDocumentsOptions, setIdDocumentsOptions] = useState([]);
+
+    // The users management route is the only one that needs to fetch roles and idDocumentTypes options for new users
+    useEffect(() => {
+        const fetchSelectOptions = async () => {
+            let rolesList = [];
+            let idDocumentsList = [];
+            try {
+                // fetch roles options
+                const rolesQuerySnapshot = await getDocs(
+                    collection(db, "roles")
+                );
+                rolesQuerySnapshot.forEach((doc) => {
+                    rolesList.push({ id: doc.id, ...doc.data() });
+                });
+                setRolesOptions(rolesList);
+
+                // fetch idDocumentTypes options
+                const idDocumentsQuerySnapshot = await getDocs(
+                    collection(db, "idDocuments")
+                );
+                idDocumentsQuerySnapshot.forEach((doc) => {
+                    idDocumentsList.push({ id: doc.id, ...doc.data() });
+                });
+                setIdDocumentsOptions(idDocumentsList);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchSelectOptions();
+    }, []);
+    console.log(rolesOptions);
+    console.log(idDocumentsOptions);
+
     const { label, onChange, options, name, value } = props; //Destructuring assignment
     return (
         <div className={styles.selectContainer}>
