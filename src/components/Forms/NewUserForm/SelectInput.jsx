0368ac14f93b7c49b8c1051eb5dd6360da659166
jdcarvajal <jdcc1308@gmail.com>
2024-05-09 16:20:57 -0500

    feat complete

--- src/components/Forms/NewUserForm/SelectInput.jsx
+++ src/components/Forms/NewUserForm/SelectInput.jsx
@@ -4,10 +4,10 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebase";
 
 const SelectInput = (props) => {
-    const [rolesOptions, setRolesOptions] = useState([]);
-    const [idDocumentsOptions, setIdDocumentsOptions] = useState([]);
+    const [roleOptions, setRoleOptions] = useState([]);
+    const [idDocumentOptions, setIdDocumentOptions] = useState([]);
 
-    // The users management route is the only one that needs to fetch roles and idDocumentTypes options for new users
+    // Hook to fetch roles and idDocumentTypes options for new users
     useEffect(() => {
         const fetchSelectOptions = async () => {
             let rolesList = [];
@@ -18,28 +18,30 @@ const SelectInput = (props) => {
                     collection(db, "roles")
                 );
                 rolesQuerySnapshot.forEach((doc) => {
-                    rolesList.push({ id: doc.id, ...doc.data() });
+                    rolesList.push({ id: doc.id, role: doc.data().role });
                 });
-                setRolesOptions(rolesList);
+                setRoleOptions(rolesList);
 
                 // fetch idDocumentTypes options
                 const idDocumentsQuerySnapshot = await getDocs(
                     collection(db, "idDocuments")
                 );
                 idDocumentsQuerySnapshot.forEach((doc) => {
-                    idDocumentsList.push({ id: doc.id, ...doc.data() });
+                    idDocumentsList.push({
+                        id: doc.id,
+                        idDocumentType: doc.data().idDocumentType,
+                    });
                 });
-                setIdDocumentsOptions(idDocumentsList);
+                setIdDocumentOptions(idDocumentsList);
             } catch (error) {
                 console.log(error);
             }
         };
         fetchSelectOptions();
     }, []);
-    console.log(rolesOptions);
-    console.log(idDocumentsOptions);
 
-    const { label, onChange, options, name, value } = props; //Destructuring assignment
+    const { label, onChange, options, name, value } = props; //Destructuring
+
     return (
         <div className={styles.selectContainer}>
             <label className={styles.labelGeneric}>{label}</label>
@@ -54,22 +56,28 @@ const SelectInput = (props) => {
                 <option hidden value="">
                     Seleccione una opción
                 </option>
+
+                {/* Conditional rendering */}
                 {name === "idDocumentType" ? (
                     <>
-                        <option value="C.C.">C.C.</option>
-                        {options.map((option, index) => (
-                            <option key={index} value={option}>
-                                {option}
+                        {/* Default option */}
+                        <option value="C.C.">C.C.</option>{" "}
+                        {idDocumentOptions.map((idDocumentOption) => (
+                            <option
+                                key={idDocumentOption.id}
+                                value={idDocumentOption.idDocumentType}
+                            >
+                                {idDocumentOption.idDocumentType}
                             </option>
                         ))}
                     </>
                 ) : name === "role" ? (
                     <>
-                        <option value="Usuario">Usuario</option>
-                        <option value="Moderador">Moderador</option>
-                        {options.map((option, index) => (
-                            <option key={index} value={option}>
-                                {option}
+                        {/* Default option */}
+                        <option value="Usuario">Usuario</option>{" "}
+                        {roleOptions.map((roleOption) => (
+                            <option key={roleOption.id} value={roleOption.role}>
+                                {roleOption.role}
                             </option>
                         ))}
                     </>
