302e4d421b182cb9c9762968af1e90e70459eb21
jdcarvajal <jdcc1308@gmail.com>
2024-05-09 19:16:12 -0500

    create UpdateIDDocumentTypeForm folder and it´s children files

---
+++ src/components/Forms/UpdateIDDocumentTypeForm/UpdateIDDocumentTypeForm.jsx
@@ -0,0 +1,11 @@
+import styles from "./UpdateIDDocumentTypeForm.module.css";
+
+const UpdateIDDocumentTypeForm = () => {
+    return (
+        <>
+            <h1>asd</h1>
+        </>
+    );
+};
+
+export default UpdateIDDocumentTypeForm;
