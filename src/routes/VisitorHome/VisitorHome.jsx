9316420405259120762f5b690dc98fb8c9e9a107
jdcarvajal <jdcc1308@gmail.com>
2024-05-10 14:59:23 -0500

    create VisitorHome route

---
+++ src/routes/VisitorHome/VisitorHome.jsx
@@ -0,0 +1,11 @@
+import styles from "./VisitorHome.module.css";
+
+const VisitorHome = () => {
+    return (
+        <>
+            <h1>asdsad</h1>
+        </>
+    );
+};
+
+export default VisitorHome;
